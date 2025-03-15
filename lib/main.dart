001deda8b701d5153670cfecaed228c7e1e2e241
Alaa Alzibda <alaa07996@gmail.com>
2025-03-15 15:27:36 +0200

    completed firebase notification integration

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'dart:io' show Platform;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:permission_handler/permission_handler.dart';
 
+import 'backend/schema/util/schema_util.dart';
 import 'components/forget_password_component/forget_password_component_model.dart';
 import 'components/login_side_component/login_side_component_model.dart';
 import 'components/otp_component/otp_component_model.dart';
 import 'general_exports.dart';
 import 'pages/exclusive_projects/exclusive_projects_model.dart';
 import 'pages/login_page/login_page_model.dart';
+import 'services/push_notification_service.dart';
 import 'structure_main_flow/flutter_mada_util.dart';
 
 bool get isAndroid => !kIsWeb && Platform.isAndroid;
@@ -27,11 +30,19 @@ void main() async {
   final appProvider = AppProvider.instance;
   await appProvider.init();
 
+  await Permission.notification.isDenied.then(
+    (bool value) {
+      if (value) {
+        Permission.notification.request();
+      }
+    },
+  );
+
+  await PushNotificationService().setupInteractedMessage();
   runApp(
     MultiProvider(
       providers: <SingleChildWidget>[
         ChangeNotifierProvider(create: (_) => appProvider),
-   
 
         // The bottom models should be deleted we don't want them globally
         ChangeNotifierProvider(
@@ -53,4 +64,5 @@ void main() async {
       child: const MyApp(),
     ),
   );
+  consoleLog(await getFcmToken() ?? '', key: 'fcmToken');
 }
